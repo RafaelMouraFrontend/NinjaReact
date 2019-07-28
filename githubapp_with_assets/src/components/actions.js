c65d132013ce8002f1692a39b7141d744dac5ef5
RafaelMouraFrontend <rafael.mouraweb@gmail.com>
2019-07-28 20:16:15 -0300

    assets errors

---
+++ githubapp_with_assets/src/components/actions.js
@@ -0,0 +1,16 @@
+'use strict'
+import React, { PropTypes } from 'react'
+
+const Actions = ({ getRepos, getStarred }) => (
+  <div className='actions'>
+    <button onClick={getRepos}>Ver Repositorios</button>
+    <button onClick={getStarred}>Ver Favorito</button>
+  </div>
+)
+
+Actions.propTypes = {
+  getRepos: PropTypes.func.isRequired,
+  getStarred: PropTypes.func.isRequired
+}
+
+export default Actions
